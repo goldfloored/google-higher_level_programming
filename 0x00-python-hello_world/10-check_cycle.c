7519d812d4a4bebbc61175bd79806781d289c114
goldfloored <goldfloored@gmail.com>
2021-04-28 09:41:07 +0000

    com

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+int check_cycle(listint_t *list)
+{
+	listint_t *current;
+	if (list == NULL)
+	{
+		return (0);
+	}
+	current = list;
+	while(current->next != NULL)
+	{
+		current = current->next;
+		if (current->next == list)
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
+
