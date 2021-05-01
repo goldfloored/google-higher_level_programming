3529884b3fa682e333865e6d84df805b6d7fcd1e
goldfloored <goldfloored@gmail.com>
2021-05-01 02:50:12 +0000

    com

--- 0x01-python-if_else_loops_functions/6-print_comb.py
+++ 0x01-python-if_else_loops_functions/6-print_comb.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-for i in range(0, 10):
-    for j in range(0, 10):
-        if ((i == 8) and (j == 9)):
-            print("{}{}".format(i, j))
-        else:
-            if (i < j):
-                print("{}{},".format(i, j), end=" ")
+for i in range(10):
+    for j in range(10):
+        if (i != j and j > i):
+            if i < 8:
+                print("{:d}{:d}".format(i, j), end=", ")
+            else:
+                print("{:d}{:d}".format(i, j), end="\n")
