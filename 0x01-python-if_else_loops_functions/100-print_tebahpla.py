21e880de0aaa6f30ebf696455aec7e456dad1485
goldfloored <goldfloored@gmail.com>
2021-05-01 02:54:33 +0000

    com

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
+j = 2
 for i in range(122, 96, -1):
-    if i % 2 != 0:
-        i = i - 32
-    print("{}".format(chr(i)), end="")
+    if j % 2 == 0:
+        print("{:c}".format(i), end='')
+    else:
+        print("{:c}".format(i).upper(), end='')
+    j += 1
