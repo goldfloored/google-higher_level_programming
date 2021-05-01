9361808707882b7ff766eae1f72100125bfca024
goldfloored <goldfloored@gmail.com>
2021-05-01 03:21:58 +0000

    com

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == '__main__':
+    import sys
+if len(sys.argv) - 1 == 0:
+    print("0 arguments.")
+else:
+    if len(sys.argv) - 1 == 1:
+        print("1 argument:")
+        print("{}: {}".format(1, sys.argv[1]))
+    else:
+        print("{:} arguments:".format(len(sys.argv) - 1))
+        for i in range(1, len(sys.argv)):
+            print("{}: {}".format(i, sys.argv[i]))
