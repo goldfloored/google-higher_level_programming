9870f1e824211e34e8f34c1569b88e04b5f5a0fa
goldfloored <goldfloored@gmail.com>
2021-06-12 22:48:50 +0000

    com

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,9 +3,6 @@
 '''
 
 
-import json
-
-
 class Base:
     '''Manage id attribute in all future classes
     '''
@@ -13,36 +10,9 @@ class Base:
     __nb_objects = 0
 
     def __init__(self, id=None):
-        '''the init method'''
+        '''__init__'''
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-
-    @staticmethod
-    def to_json_string(list_dictionaries):
-        '''convert to a json string'''
-        if list_dictionaries is None:
-            return "[]"
-        else:
-            return json.dumps(list_dictionaries)
-
-    @classmethod
-    def save_to_file(cls, list_objs):
-        '''save to json file'''
-        if list_objs is None:
-            list_objs = []
-        l = []
-        for obj in list_objs:
-            l.append(cls.to_dictionary(obj))
-        with open('{}.json'.format(cls.__name__), 'w') as file:
-            file.write(cls.to_json_string(l))
-
-    @staticmethod
-    def from_json_string(json_string):
-        '''convert from json string'''
-        if json_string is None:
-            return []
-        else:
-            return json.loads(json_string√)
