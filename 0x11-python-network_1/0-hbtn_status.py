6fc563ca77574300445544f285f53df9e837b790
goldfloored <goldfloored@gmail.com>
2021-11-02 15:34:45 +0100

    com

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
-"""Fetches https://intranet.hbtn.io/status."""
-import urllib.request
+"""Python script that fetches https://intranet.hbtn.io/status"""
 
-
-if __name__ == "__main__":
-    request = urllib.request.Request("https://intranet.hbtn.io/status")
-    with urllib.request.urlopen(request) as response:
-        body = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode("utf-8")))
+if __name__ == '__main__':
+    import urllib.request
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        url = response.read()
+    print("Body response:")
+    print('\t- type: {}'.format(type(url)))
+    print('\t- content: {}'.format(url))
+    print('\t- utf8 content: {}'.format(url.decode('utf-8')))
